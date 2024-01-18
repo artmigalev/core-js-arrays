5df4b23b326b68ef842c695de9bcd5b3a77f9a1b
david-anav <art_migalev@yahoo.com>
2024-01-18 10:57:47 +0300

    feat: 7/42

--- exemple.js
+++ exemple.js
@@ -1,5 +1,6 @@
-console.log(
-  findElement(['Ace', 10, true], 10) /* 1*/,
-  findElement(['Array', 'Number', 'string'], 'Date') /* -1*/,
-  findElement([0, 1, 2, 3, 4, 5], 5) /* 5*/
-);
+function findAllOccurrences(arr, item) {
+  const result = arr.filter((el) => el === item);
+  return result.length;
+}
+
+console.log(findAllOccurrences([0, 0, 1, 1, 1, 2], 1));
