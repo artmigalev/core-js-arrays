514645ea6af9ea6cf4db690d8fce5f4b09a4b2c5
david-anav <art_migalev@yahoo.com>
2024-01-18 10:30:39 +0300

    fix6/43

--- exemple.js
+++ exemple.js
@@ -1,18 +1,5 @@
-function sumArrays( arr1, arr2) {
-  if (Array.of(arr1,arr2).some(arr => {return arr.length === 0})){
-    return arr1.length > arr2.length ? arr1: arr2;
-  }
-  const lengthAR = arr1.length > arr2.length ? arr1.length : arr2.length;
-
-  return Array.from({ length: lengthAR },(_, i) => {
-    if( arr1[i] === undefined) {
-      return arr2[i];
-    }else if(arr2[i] === undefined){
-      return arr1[i]
-    }
-
-    return arr1[i] + arr2[i]
-  })
-}
-console.log(sumArrays([-1, 0, 1, 4, 5, 5, 5], [6]));
-
+console.log(
+  findElement(['Ace', 10, true], 10) /* 1*/,
+  findElement(['Array', 'Number', 'string'], 'Date') /* -1*/,
+  findElement([0, 1, 2, 3, 4, 5], 5) /* 5*/
+);
