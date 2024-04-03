1c62e55492700acb153daa2e621181823fb0b392
david-anav <art_migalev@yahoo.com>
2024-04-03 22:13:18 +0300

    43/6/1

--- exemple.js
+++ exemple.js
@@ -1,7 +1,18 @@
-function propagateItemsByPositionIndex(arr) {
-  return arr.flatMap((el, i) => {
-    return Array.from({ length: i + 1 }).fill(el);
-  });
+function shiftArray(arr, n) {
+  if (n > 0) {
+    for (let i = 0; i < n; i++) {
+      arr.unshift(arr.pop())
+    }
+  }
+  if (n < 0) {
+    for (let i = 0; i > n; i--) {
+      console.log(i);
+      arr.push(arr.shift());
+      console.log(arr)
+    }
+  }
+  return arr;
 }
 
-console.log(propagateItemsByPositionIndex(['a', 'b', 'c', null]));
+console.log(shiftArray([1, 2, 3, 4, 5], 2));
+console.log(shiftArray(['a', 'b', 'c', 'd'], -1));
--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -580,8 +580,18 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n > 0) {
+    for (let i = 0; i < n; i += 1) {
+      arr.unshift(arr.pop());
+    }
+  }
+  if (n < 0) {
+    for (let i = 0; i > n; i -= 1) {
+      arr.push(arr.shift());
+    }
+  }
+  return arr;
 }
 
 /**
