fccad816898d5762a2305c6a868f6e0ca20e36b2
artmigalev <art_migalev@yahoo.com>
2024-12-05 21:47:18 +0300

    46

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -582,10 +582,8 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(arr, n) {
-  n= n % arr.length;
-  if (n < 0) n = n + arr.length;
-  return arr.slice(n).concat(arr.slice(0, n));
+function shiftArray(/* arr, n */) {
+  throw new Error('Not implemented');
 }
 
 /**
